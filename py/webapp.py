2f9a4a51732d63979d1b260cc326b6d897551261
xjanua <tonyisbest@gmail.com>
2015-11-10 12:40:14 -0500

    +Files
    Bootstrap, Webapp.py, index.html

---
+++ py/webapp.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python
+import bottle as bt
+import route, run, template
+
+@bt.route('/<filename>')
+def index(filename):
+    return bt.static_file(filename, root='/PyPlay')
+
+#run(host='104.131.246.240', port=8083, debug=True)
+bt.debug(True)
+
+if __name__ == '__main__':    
+    bt.run()
