b39dbe7a1ba68681463e5c6a2f5bdf85287a33b3
xjanua <tonyisbest@gmail.com>
2015-11-10 14:21:56 -0500

    idk

--- py/webapp.py
+++ py/webapp.py
@@ -1,13 +1,13 @@
 #!/usr/bin/env python
 import bottle as bt
 
-@bt.route('/PyPlay/<filename>', method = "GET")
-def index(filename):
-    f = open('../'+filename)
+@bt.route('/', method = "GET")
+def index():
+    f = open('../index.html')
     return f.read()
 
-#run(host='104.131.246.240', port=8083, debug=True)
 bt.debug(True)
 
 if __name__ == '__main__':    
-    bt.run(host='104.131.246.240', port=8080, debug=True)
+#    bt.run(host='104.131.246.240', port=8080, debug=True)
+    bt.run(port=80)
