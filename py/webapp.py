9cc7d0f9f4ce1f3bb46e86df618d632707b23c77
xjanua <tonyisbest@gmail.com>
2015-11-10 15:33:22 -0500

    css?

--- py/webapp.py
+++ py/webapp.py
@@ -15,9 +15,15 @@ def index():
     f = open('../index.html')
     return f.read()
 
+@bt.route('/root/PyPlay/style.css')
+def stylesheet(filename):
+    f = open('../css/style.css')
+    return f.read()
+
 bt.debug(True)
 
 #Set your host(Web Server IP) here, port 80 will only require you to type in the Web Server IP to access the web page
 
 if __name__ == '__main__':
-    bt.run(host='104.131.246.240', port=80, debug=True)
+#    bt.run(host='104.131.246.240', port=8080, debug=True)
+    bt.run(port=8080)
